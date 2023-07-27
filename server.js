a22acd20f890f27411e3345a3d1ef3a766f258b1
MSWSal <sahansalgado10@gmail.com>
2023-07-27 11:53:52 +0530

    part1

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+const port=5000
+
+app.listen(port, ()=>{
+    console.log("server running on port:"+port)
+})
