d75e2f5d331c3db2fc4b9e53d3fd528b860e729d
MSWSal <sahansalgado10@gmail.com>
2023-07-27 13:12:05 +0530

    part5

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ const openai = new OpenAIApi(new Configuration({
     apiKey: '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'
 }))
 
+app.post('/chat', async (req, res)=>{
+    try{
+        const response= await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {role: "user", content: req.body.question}
+            ]
+        })
+
+    }catch(e){
+        res.status(400).json({message: e.message})
+    }
+})
+
 const port=5000
 
 app.listen(port, ()=>{
